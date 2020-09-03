4dd3109a7c4618dcecf03f3b6c0f85b6bfa2ff44
blueskyleader01 <blueskyleader01@hotmail.com>
2020-09-02 21:40:13 -0400

    step 3 done

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -2,7 +2,9 @@ package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false) //this will block Authors API, no output at http://localhost:8080/authors
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
 }
