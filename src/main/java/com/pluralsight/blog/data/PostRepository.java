a945df8c2d0f5c9263fa4fba2ecb500380271c0c
blueskyleader01 <blueskyleader01@hotmail.com>
2020-09-02 21:11:01 -0400

    step 2 done

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -2,5 +2,14 @@ package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface PostRepository extends JpaRepository<Post, Long>{};
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long>{
+    @RestResource(rel="contains-title", path="containsTitle")
+    public List<Post> findByTitleContaining(String title);
+    //for search post by title, in url http://localhost:8080/posts/search/findByTitleContaining?title=string
+    //if don't want the path to be too long, redefine it by using @RestResource(rel="contains-title", path="containsTitle")
+
+};
